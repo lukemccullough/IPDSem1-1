bf09ddc4921b27b96dc82bb9f3ee38c68aba5282
lukemccullough <222303@shakopeeschools.org>
2019-11-22 14:12:19 -0600

    Update team14.py

--- team14.py
+++ team14.py
@@ -6,11 +6,24 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'T14' # Only 10 chars displayed.
+strategy_name = 'Collude then First Move'
+strategy_description = 'Collude for first 25 moves, then use first move.'
     
 def move(my_history, their_history, my_score, their_score):
+    if len(my_history) == 0:  
+        return 'c'
+    if len(my_history) >= 25:
+        return their_history[1]
+    else:
+        return 'c'
+    
+            
+        
+    
+    
+    
+        
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
@@ -25,8 +38,8 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+
+        
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
